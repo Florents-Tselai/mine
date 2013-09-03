762b26776c4e6bee8a01bd1f30a11602e465ac2f
Florents Tselai <tselai@dmst.aueb.gr>
2013-09-03 18:28:11 +0300

    No need for these

--- mine/mine.py
+++ mine/mine.py
@@ -162,8 +162,3 @@ def GetProbabilityDistribution(P, n):
     """
     d = GetPartitionGroups(P)    
     return [float( len(d[k])) / float(n) for k in sorted(d.keys())]
-
-
-D = [(1,1), (1,2), (1,3), (1,4), (2,3), (2,4), (3,5), (4,6), (5,6), (6,6), (7,5), (8,3), (9,2), (9,1)]
-x_partition = [1.8, 2.2, 7.8, 8.2]
-y_partition = [2.5, 4.6]
