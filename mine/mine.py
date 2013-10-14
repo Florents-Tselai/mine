1935e7b9c3a65f5df19574880a496868f9cd1c47
Florents Tselai <tselai@dmst.aueb.gr>
2013-10-14 14:47:27 +0300

    Improve tests and move everything to single module

--- mine/mine.py
+++ mine/mine.py
@@ -17,11 +17,22 @@ from collections import defaultdict, Mapping
 from copy import copy
 from itertools import chain, tee, izip
 from math import log, floor
-from mine_utils import *
-from mine_computations import *
+
 import matplotlib.pyplot as plt
 import numpy as np
 
+p_x, p_y = lambda p: p[0], lambda p: p[1]
+
+def get_rightest_point(points): return max(points, key=p_x)
+
+def get_leftest_point(points): return min(points, key=p_x)
+
+def get_uppest_point(points): return max(points, key=p_y)
+
+def get_downest_point(points): return min(points, key=p_y)
+
+def last_abscissa(x_bin): return p_x(get_rightest_point(x_bin))
+def last_ordinate(y_bin): return p_y(get_uppest_point(y_bin))
 
 def ApproxMaxMI(D, x, y, k_hat):
     assert x > 1 and y > 1 and k_hat > 1 
@@ -187,3 +198,142 @@ def OptimizeXAxis(D, Q, x, k_hat):
     for l in xrange(k + 1, x + 1): I[k][l] = I[k][k]
     
     return I[k][2:x + 1]
+
+def entropy(probs): 
+    return -sum(p * log(p, 2) for p in probs if p > 0)
+                 
+def I(P_ordinals, Q_map):
+    return HQ(Q_map) + HP(P_ordinals) - HPQ(P_ordinals, Q_map)
+
+def HP(Dx, P_ordinals):
+    assert is_sorted_increasing_by(Dx, 'x')
+    
+    #Number of points in the partition 
+    m = P_ordinals[-1] + abs(P_ordinals[0])
+    return entropy(np.array(GetPartitionHistogram(Dx, P_ordinals)) / float(m))
+
+def HQ(Q_map):
+    n = len(Q_map)
+    temp = GroupPointsByPartition(Q_map)
+    return entropy([len(temp[k]) / float(n) for k in temp])
+
+def HPQ(P_ordinals, Q_map):
+    Dx = sort_D_increasing_by(Q_map.keys(), 'x')
+    
+    m = P_ordinals[-1] + abs(P_ordinals[0])
+    return entropy(np.array(GetGridHistogram(Q_map, P_ordinals)) / float(m))
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
+
+def is_sorted_increasing_by(D, increasing_by='x'):
+    assert increasing_by == 'x' or increasing_by == 'y'
+    
+    if increasing_by == 'x':
+        return all(p_x(D[i]) <= p_x(D[i + 1]) for i in xrange(len(D) - 1))
+    else:
+        return all(p_y(D[i]) <= p_y(D[i + 1]) for i in xrange(len(D) - 1))
+
+def sort_D_increasing_by(D, increasing_by='x'):
+    assert increasing_by == 'x' or increasing_by == 'y'
+    
+    return sorted(D, key=p_x) if increasing_by == 'x' else sorted(D, key=p_y)
+
+def GetPartitionOrdinalsFromMap(D, P, axis='x'):
+    assert is_sorted_increasing_by(D, axis)
+    
+    P_tilde = GroupPointsByPartition(P)
+    
+    if axis == 'x':
+        return [D.index(get_leftest_point(P_tilde[0])) - 1] + [D.index(get_rightest_point(P_tilde[k])) for k in sorted(P_tilde.keys())]
+    elif axis == 'y':
+        return [D.index(get_downest_point(P_tilde[0])) - 1] + [D.index(get_uppest_point(P_tilde[k])) for k in sorted(P_tilde.keys())]
+  
+def GetPartitionMapFromOrdinals(D, ordinals, axis='x'):
+    assert is_sorted_increasing_by(D, axis)
+    
+    to_be_binned = range(len(D))
+        
+    #Translate Reshef's convention to adhere to Numpy's one
+    bins = [o+1 for o in ordinals[:-1]]  
+    #Assign point indices to bins formed by the partition ordinals
+    map = {D[point_index]:partition-1 for point_index, partition in enumerate(np.digitize(to_be_binned, bins))}
+    
+    return map
+
+def GroupPointsByPartition(P):
+    """
+    P : point -> Partition index
+    Returns
+    d : partition index -> points
+    
+    Example:
+    P = 
+    {
+    p1 -> 1
+    p2 -> 2
+    P3 -> 1
+    p4 -> 2
+    }
+    
+    Returns
+    d = 
+    {
+    1 -> [p1, p3]
+    2 -> [p2, p4]
+    }
+      
+    """
+    d = defaultdict(list)
+    for k, v in P.iteritems(): 
+        d[v].append(k)
+    return dict(d)
+
+def GetPartitionHistogram(D, ordinals, axis='x'):
+    assert is_sorted_increasing_by(D, axis)
+    return [p[1]+1 if p[0]<0 else p[1]-p[0] for p in pairwise(ordinals)]
+
+def visualize(x_axis_parition={}, y_axis_partition={}, step=0.2):
+    points = set(chain(x_axis_parition.iterkeys(), y_axis_partition.iterkeys()))
+    
+    x_axis_parition = GroupPointsByPartition(x_axis_parition)
+    y_axis_partition = GroupPointsByPartition(y_axis_partition)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    # Scatter points
+    ax.scatter(map(p_x, points), map(p_y, points))
+    
+    x_bin_edge = lambda x_bin :last_abscissa(x_bin) + step
+    y_bin_edge = lambda y_bin:  last_ordinate(y_bin) + step
+    
+    x_ticks = map(x_bin_edge, x_axis_parition.itervalues())
+    y_ticks = map(y_bin_edge, y_axis_partition.itervalues())
+    
+    ax.get_xaxis().set_ticks(x_ticks)
+    ax.get_yaxis().set_ticks(y_ticks)
+    
+    # Format grid appearance
+    ax.grid(True, alpha=0.5, color='red', linestyle='-', linewidth=1.5)
+    
+    x_partition_size = len(x_axis_parition.values())
+    y_partition_size = len(y_axis_partition.values())
+    plt.title(str(x_partition_size) + ' - by - ' + str(y_partition_size) + ' Grid')
+    plt.show()
+
+def GetGridHistogram(Q, P_ordinals):
+    rows = GroupPointsByPartition(Q)
+    
+    Dx = sort_D_increasing_by(Q.keys(), 'x')
+    
+    histogram = []
+    for p in pairwise(P_ordinals):
+        p1, p2 = p[0], p[1]
+        l = Dx[(p1+1):(p2+1)]
+        for r in xrange(len(rows)):
+            histogram.append(sum(1 for point in l if point in rows[r]))
+    return histogram
--- mine/test_mine.py
+++ mine/test_mine.py
@@ -117,5 +117,349 @@ def test_ApproxCharacteristicMatrix():
     M = ApproxCharacteristicMatrix(D, B, c)
     print M
     
+def test_HP():
+    D = [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 0), (6, 4)]
+    Dx = sort_D_increasing_by(D, 'x')
+    
+    P1_ordinals = [-1, 0, 2, 5, 6]
+    # We should get the following x-axis partition
+    """
+      4  |   |     |x
+      3  |   |  x  |
+      2  |   |x    |
+         *----+---+-----+-
+      1  |x x|     |
+         *----+---+-----+-
+      0 x|   |    x|
+        0|1 2|3 4 5 6
+         |   |     |
+     """
+    assert HP(Dx, P1_ordinals) == entropy([1./7, 2./7 , 3./7,1./7])
+    
+    assert HP(Dx, [0,2,5]) == entropy([2./5, 3./5])
+    
+    assert HP(Dx, [-1,0]) == entropy([1./1])
+    
+    assert HP(Dx, [-1, 5, 6]) == entropy([6./7 , 1./7])
+    
+def test_HQ():
+    """
+      4  |   |     |x
+      3  |   |  x  |
+      2  |   |x    |
+         *----+---+-----+-
+      1  |x  |     |
+         *----+---+-----+-
+      0 x|   |    x|
+        0|1 2|3 4 5 6
+         |   |     |
+     """
+     
+    assert HQ(
+            {
+             (0,0): 0,
+             (5,0): 0,
+             (1,1): 1,
+             (3,2): 2,
+             (4,3): 2,
+             (6,4): 2
+            }
+            ) == entropy([2./6, 1./6, 3./6])
+            
+    """
+      4  |   |     |x
+      3  |   |  x  |
+      2  |   |x    |
+         *----+---+-----+-
+      1  |x x|     |
+      0 x|   |    x|
+        0|1 2|3 4 5 6
+         |   |     |
+     """
+    assert HQ(
+            {
+             (0,0): 0,
+             (5,0): 0,
+             (1,1): 0,
+             (2,1): 0,
+             (3,2): 1,
+             (4,3): 1,
+             (6,4): 1
+            }
+            ) == entropy([4./7, 3./7])
+    
+def test_HPQ():
+    """
+      4  |   |     |x
+      3  |   |  x  |
+      2  |   |x    |
+         *----+---+-----+-
+      1  |x x|     |
+         *----+---+-----+-
+      0 x|   |    x|
+        0|1 2|3 4 5 6
+         |   |     |
+     """
+    P_ordinals = [-1, 0, 2, 5]
+    Q_map = {
+             (0,0): 0,
+             (5,0): 0,
+             
+             (1,1): 1,
+             (2,1): 1,
+             
+             (3,2): 2,
+             (4,3): 2,
+             (6,4): 2
+             }
+    
+    assert HPQ(P_ordinals, Q_map) == entropy([1./6, 0./6, 0./6, 0./6, 2./6, 1./6, 0./6, 2./6])
+ 
+def test_GetPartitionOrdinalsFromMap():
+    # Points sorted by increasing x-value
+    D = [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 0), (6, 4)]
+    
+    # Given the following x-axis partition
+    """
+      4  |   |     |x
+      3  |   |  x  |
+      2  |   |x    |
+         *----+---+-----+-
+      1  |x x|     |
+         *----+---+-----+-
+      0 x|   |    x|
+        0|1 2|3 4 5 6
+         |   |     |
+     """
+     
+    P1 = {
+          (0, 0): 0,
+          (1, 1): 1,
+          (2, 1): 1,
+          (3, 2): 2,
+          (4, 3): 2,
+          (5, 0): 2,
+          (6, 4): 3
+         }
+    
+    partition_size = len(set(P1.values()))
+    assert partition_size == 4
+    
+    expected_ordinals = [-1, 0, 2, 5, 6]
+    assert len(GetPartitionOrdinalsFromMap(D, P1)) == partition_size + 1
+    assert GetPartitionOrdinalsFromMap(D, P1) == expected_ordinals
+    
+    # Another test
+    """
+      4            |x
+      3         x  |
+      2       x    |
+         *----+---+-----+-
+      1   x x      |
+         *----+---+-----+-
+      0 x         x|
+        0 1 2 3 4 5 6
+                   |
+     """
+    P2 = {
+          (0, 0): 0,
+          (1, 1): 0,
+          (2, 1): 0,
+          (3, 2): 0,
+          (4, 3): 0,
+          (5, 0): 0,
+          (6, 4): 1
+         }
+    partition_size = len(set(P2.values()))
+    assert partition_size == 2
+    
+    expected_ordinals = [-1, 5, 6]
+    assert len(GetPartitionOrdinalsFromMap(D, P2)) == partition_size + 1
+    assert GetPartitionOrdinalsFromMap(D, P2) == expected_ordinals
+    
+    D = [(0,0), (10,10), (20,20), (30,30), (40,40), (50,50), (60,60), (70,70), (80,80), (90,90)]
+    
+    Q = {
+         (0,0): 0, 
+         (10,10): 0, 
+         (20,20): 0, 
+         (30,30): 1, 
+         (40,40): 1, 
+         (50,50): 1, 
+         (60,60): 2, 
+         (70,70): 2, 
+         (80,80): 3, 
+         (90,90):4
+         }
+    
+    expected_ordinals = [-1, 2, 5, 7, 8, 9]
+    assert GetPartitionOrdinalsFromMap(D, Q) == expected_ordinals
+    
+def test_GetParitionMapFromOrdinals():
+    # Points sorted by increasing x-value
+    D = [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 0), (6, 4)]
+    
+    # Given the following x-axis points ordinals
+    ordinals = [-1, 0, 2, 5, 6]
+    # We should get the following x-axis partition
+    """
+      4  |   |     |x
+      3  |   |  x  |
+      2  |   |x    |
+         *----+---+-----+-
+      1  |x x|     |
+         *----+---+-----+-
+      0 x|   |    x|
+        0|1 2|3 4 5 6
+         |   |     |
+     """
+    # Which is described by this dictionary
+    P = {(0, 0): 0,
+         (1, 1): 1,
+         (2, 1): 1,
+         (3, 2): 2,
+         (4, 3): 2,
+         (5, 0): 2,
+         (6, 4): 3
+         }
+    assert GetPartitionMapFromOrdinals(D, ordinals) == P
+    
+    # Now test with two ordinals
+    
+    """
+      4      |     |x
+      3      |  x  |
+      2      |x    |
+         *----+---+-----+-
+      1  x x |     |
+         *----+---+-----+-
+      0 x    |    x|
+        0 1 2|3 4 5 6
+             |     |
+     """
+     
+    ordinals = [-1, 2, 5, 6]
+    P = {(0, 0): 0,
+         (1, 1): 0,
+         (2, 1): 0,
+         (3, 2): 1,
+         (4, 3): 1,
+         (5, 0): 1,
+         (6, 4): 2
+         }
+    assert GetPartitionMapFromOrdinals(D, ordinals) == P
+    
+def test_GetPartitionHistogram():
+    """
+      4      |     |x
+      3      |  x  |
+      2      |x    |
+         *----+---+-----+-
+      1  x x |     |
+         *----+---+-----+-
+      0 x    |    x|
+        0 1 2|3 4 5 6
+             |     |
+     """
+    D = [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 0), (6, 4)]
+
+    ordinals = [-1, 2, 5, 6]
+    assignments = GetPartitionHistogram(D, ordinals)
+    assert list(assignments) == [3,3,1]
+    
+    ############ Another Test Case ##############
+    
+    
+     # Points sorted by increasing x-value
+    D = [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 0), (6, 4)]
+    
+    # Given the following x-axis points ordinals
+    ordinals = [-1, 0, 2, 5, 6]
+    # We should get the following x-axis partition
+    """
+      4  |   |     |x
+      3  |   |  x  |
+      2  |   |x    |
+         *----+---+-----+-
+      1  |x x|     |
+         *----+---+-----+-
+      0 x|   |    x|
+        0|1 2|3 4 5 6
+         |   |     |
+     """
+    assignments = GetPartitionHistogram(D, ordinals)
+    assert list(assignments) == [1,2,3,1]
+    
+    #Other test case
+    D = [(0,0), (10,10), (20,20), (30,30), (40,40), (50,50), (60,60), (70,70), (80,80), (90,90)]
+    
+    Q = {
+         (0,0): 0, 
+         (10,10): 0, 
+         (20,20): 0, 
+         (30,30): 1, 
+         (40,40): 1, 
+         (50,50): 1, 
+         (60,60): 2, 
+         (70,70): 2, 
+         (80,80): 3, 
+         (90,90):4
+         }
+    
+    assignments = GetPartitionHistogram(D, GetPartitionOrdinalsFromMap(D, Q, 'y'), 'y')
+    assert assignments == [3,3,2,1,1]
+ 
+def test_visualize():
+    # Albanese dataset
+    # http://mpba.fbk.eu/sites/mpba.fbk.eu/files/albanese12cmine_suppmat.pdf#page=3
+    D = [(1, 1), (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 5), (4, 6), (5, 6), (6, 6), (7, 5), (8, 3), (9, 2), (9, 1)]
+    Q = EquipartitionYAxis(D, y=3)
+    P = GetClumpsPartition(D, Q)
+    
+    visualize(P, Q)   
+ 
+def test_GetGridHistogram():
+    """
+      4  |   |     |x
+      3  |   |  x  |
+      2  |   |x    |
+         *----+---+-----+-
+      1  |x x|     |
+         *----+---+-----+-
+      0 x|   |    x|
+        0|1 2|3 4 5 6
+         |   |     |
+    """
+    D = [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 0), (6, 4)]
+    Dy = sort_D_increasing_by(D, 'y')
+    Dx = sort_D_increasing_by(D, 'x')
+    
+    Q1 = {
+         (0,0): 0,
+         (5,0): 0,
+         (1,1): 1,
+         (2,1): 1,
+         (3,2): 2,
+         (4,3): 2,
+         (6,4):2
+         }
+    
+    P1 = [-1,0,2,5,6]
+    
+    assert GetGridHistogram(Q1,P1) == [1, 0, 0, 0, 2, 0, 1, 0, 2, 0, 0, 1]
+    
+    P2 = [-1, 0, 5, 6]
+    assert GetGridHistogram(Q1,P2) == [1, 0, 0, 1, 2, 2, 0, 0, 1]
+    
+    
     
 test_ApproxCharacteristicMatrix()
+
+test_GetPartitionOrdinalsFromMap()
+test_GetParitionMapFromOrdinals()
+test_GetPartitionHistogram()
+test_GetGridHistogram()
+
+test_HP()
+test_HQ()
+test_HPQ()
