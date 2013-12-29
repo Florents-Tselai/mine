074eacc2392d838860d3f7b1cd220f8456630727
Florents Tselai <f.tselai@intelen.com>
2013-12-29 21:11:07 +0200

    Entropy and Mutual Information computations

--- mine/computations.py
+++ mine/computations.py
@@ -1,9 +1,8 @@
 import numpy as np
+from sklearn.metrics import mutual_info_score
 
 EPSILON = 1e-8
 
-
-    
 def H(distribution):
     def entropy(P):
         '''
@@ -15,10 +14,18 @@ def H(distribution):
         assert h <= np.log2(len(P)) + EPSILON
         return h
     # Flatter a 2-dim (grid histogram array)
-    return entropy(distribution.flatten())
+    return entropy(distribution.ravel())
+
 
 def getXDistribution(grid_histogram):
     return np.sum(grid_histogram, axis=0)
 
 def getYDistribution(grid_histogram):
     return np.sum(grid_histogram, axis=1)
+
+def I(grid_histogram):
+    x_distribution = getXDistribution(grid_histogram)
+    y_distribution = getYDistribution(grid_histogram)
+    joint_distribution = grid_histogram.ravel()
+    return H(x_distribution) + H(y_distribution) - H(joint_distribution)
+
--- mine/test_computations.py
+++ mine/test_computations.py
@@ -8,29 +8,36 @@ class computations_test(unittest.TestCase):
     def setUp(self):        
         #Example from wiki https://en.wikipedia.org/wiki/Marginal_distribution
         # Appears also in Cover et.al. book pg. 18
-        self.example = array([[4./32, 2./32, 1./32, 1./32], 
-                                 [2./32, 4./32, 1./32, 1./32], 
-                                 [2./32, 2./32, 2./32, 2./32], 
-                                 [8./32, 0., 0., 0.]])
+        self.wiki_example = array([[4./32, 2./32, 1./32, 1./32], 
+                                   [2./32, 4./32, 1./32, 1./32], 
+                                   [2./32, 2./32, 2./32, 2./32], 
+                                   [8./32, 0., 0., 0.]])
+        
+        # Example from Guttler's presentation
+        self.guttler_example = array([[1./20, 1./20, 1./20], 
+                                      [3./20, 5./20, 2./20], 
+                                      [3./20, 2./20, 2./20]])
 
-        assert np.sum(self.example) == 1.
+        assert np.sum(self.wiki_example) == 1.
         
     def test_H(self):
-        X = getXDistribution(self.example)
+        X = getXDistribution(self.wiki_example)
         assert H(X) == 7./4
-        Y = getYDistribution(self.example)
+        Y = getYDistribution(self.wiki_example)
         assert H(Y) == 2.
         
-        #Joint distribution
-        assert H(self.example) == 27./8
+        #Joint entropy
+        assert H(self.wiki_example) == 27./8
         
     def test_getXDistribution(self):
-        X = np.sum(self.example, axis=0)
-        assert_array_equal(X, array([1./2, 1./4, 1./8, 1./8]))
+        assert_array_equal(getXDistribution(self.wiki_example), array([1./2, 1./4, 1./8, 1./8]))
         
     def test_getYDistribution(self):
-        Y = np.sum(self.example, axis=1)
-        assert_array_equal(Y, array([1./4, 1./4, 1./4, 1./4]))
+        assert_array_equal(getYDistribution(self.wiki_example), array([1./4, 1./4, 1./4, 1./4]))
+        
+    def test_I(self):
+        assert I(self.wiki_example) == 3./8
+        
 
 if __name__ == '__main__':
     unittest.main()
