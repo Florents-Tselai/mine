b95067be5d657f327e9b65c5d11ef7fd5c924249
Florents Tselai <f.tselai@intelen.com>
2014-03-04 15:54:53 +0200

    Add mind map

---
+++ doc/MINE.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1393941195253" ID="ID_839841935" MODIFIED="1393941202711" TEXT="MINE"/>
+</map>
