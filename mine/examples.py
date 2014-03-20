fcb004626cfd95c80fafd9d72db854dec88143eb
Florents Tselai <f.tselai@intelen.com>
2014-03-20 12:12:18 +0200

    Add function to plot char matrix

--- mine/examples.py
+++ mine/examples.py
@@ -1,6 +1,6 @@
 from utils import *
 from mine import *
-
+from matplotlib import cm
 
 def plot_example_grid():
     x = np.arange(20)
@@ -9,6 +9,29 @@ def plot_example_grid():
     q = m.equipartition_y_axis(m.Dy, 3)
     p, _ = m.get_clumps_partition(q)
     plot_partitions(p, q, 'example_grid.png')
+
+def plot_char_matrix_surface(m):
+    x = np.arange(m.shape[0])
+    y = np.arange(m.shape[1])
+    xx, yy = np.meshgrid(x,y)
+
+    @np.vectorize
+    def char_value(x,y):
+        return m[x][y]
+
+
+    z = char_value(xx,yy)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection='3d')
+
+    surf = ax.plot_surface(x,y,z, rstride=1, cstride=1, cmap=cm.coolwarm,
+            linewidth=0, antialiased=False)
+    ax.set_zlim3d(-1.01, 1.01)
+
+    fig.colorbar(surf, shrink=0.5, aspect=10)
+
+    plt.show()
 
 
 if __name__ == "__main__":
