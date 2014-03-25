b338e541f6b2bbd375ee0c41b9800b7cf0fd9775
Florents Tselai <florents.tselai@gmail.com>
2014-03-25 16:53:05 +0200

    Implement get clumps partition

--- mine/mine.cpp
+++ mine/mine.cpp
@@ -18,10 +18,9 @@ PRINT_ELEMENTS(const vector <T> &v)
     cout << endl;
 }
 
-map<Point,int>
+map<const Point *,int>
 equipartition_y_axis(const vector<Point> &d, int y) {
     int n = d.size();
-    map<Point,int> q;
     PRINT_ELEMENTS(d);
     vector<const Point*> dy(n);
     //TODO REFACTOR: use transform with lambda and back_inserter
@@ -29,6 +28,8 @@ equipartition_y_axis(const vector<Point> &d, int y) {
     for (int i = 0; i < n; i++)
         dy[i] = &(d[i]);
     sort(dy.begin(), dy.end(), less_y);
+
+    map<const Point*,int> q;
 
     double desiredRowSize = double(n) / y;
     int i = 0;
@@ -52,12 +53,8 @@ equipartition_y_axis(const vector<Point> &d, int y) {
             desiredRowSize = temp1 / temp2;
         }
 
-        for (int j=0; j<s; j++) {
-            Point p = *dy[i+j];
-            cout << p << ", " << currRow << endl;
-            //TODO BUG: insert in q
-
-        }
+        for (int j=0; j<s; j++)
+            q.insert(make_pair(dy[i+j], currRow));
 
         i += s;
         sharp += s;
@@ -70,12 +67,72 @@ equipartition_y_axis(const vector<Point> &d, int y) {
 void test_equipartition_y_axis() {
     Point p[] = {{1,1}, {1,2}, {9,2}, {9,1}, {1,3}, {1,4}, {2,3}, {2,4}, {8,3}, {3,5}, {4,6}, {5,6}, {6,6}, {7,5} };
     vector <Point> points(p, p + 14);
-    equipartition_y_axis(points, 3);
+    map<const Point*, int> q = equipartition_y_axis(points, 3);
+}
 
+map<const Point *,int>
+get_clumps_partition(const vector<Point> &d, const map<const Point *,int> &q) {
+    int n = d.size();
+    map<const Point *,int> q_tilde;
+    q_tilde.insert(q.begin(), q.end());
+
+    vector<const Point*> dx(n);
+    //TODO REFACTOR: use transform with lambda and back_inserter
+    //TODO OPTIMIZATION: exploit y ordering
+    for (int i = 0; i < n; i++)
+        dx[i] = &(d[i]);
+    sort(dx.begin(), dx.end(), less_x);
+
+
+    int i = 0;
+    int c = -1;
+
+    while (i<n) {
+        int s = 1;
+        bool flag = false;
+        for (int j=i+1; j<n; j++) {
+            if (dx[i]->x == dx[j]->x) {
+                s++;
+                if (q_tilde[dx[i]] != q_tilde[dx[j]]) {
+                    flag = true;
+                }
+            }
+            else {
+                break;
+            }
+        }
+        if ((s>1) & flag) {
+            for (int j=0; j<s; j++)
+                q_tilde.insert(make_pair(dx[i+j], c));
+            c--;
+        }
+        i += s;
+    }
+
+    i = 0;
+    map<const Point *,int> p;
+    p.insert(make_pair(dx[0], i));
+
+    for(int j=1; j<n; j++) {
+        if (q_tilde[dx[j]] !=q_tilde[dx[j-1]]) {
+            i++;
+        }
+        p.insert(make_pair(dx[j], i));
+        cout << dx[j] << " " << i << endl;
+    }
+    return p;
+}
+
+void test_get_clumps_partition() {
+    Point p[] = {{1,1}, {1,2}, {9,2}, {9,1}, {1,3}, {1,4}, {2,3}, {2,4}, {8,3}, {3,5}, {4,6}, {5,6}, {6,6}, {7,5} };
+    vector <Point> points(p, p + 14);
+    map<const Point*, int> q = equipartition_y_axis(points, 3);
+    get_clumps_partition(points, q);
 }
 
 void run_tests() {
     test_equipartition_y_axis();
+    test_get_clumps_partition();
 }
 
 int main(void) {
